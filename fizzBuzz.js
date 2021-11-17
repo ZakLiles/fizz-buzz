be75ba6fd4c9b1a1c407dc7e72ab2691aa588d01
Zak Liles <zak.liles@gmail.com>
2021-11-16 19:21:40 -0700

    Challenge Completed

---
+++ fizzBuzz.js
@@ -0,0 +1,14 @@
+// Write your solution below this line:
+for (let i = 1; i <= 50; i++){
+    if (i % 3 === 0){
+      if (i % 5 === 0){
+        console.log("fizzbuzz");
+      } else {
+      console.log("fizz");
+      }
+    } else if (i % 5 === 0){
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
